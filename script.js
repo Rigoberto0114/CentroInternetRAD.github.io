791c618c7c3235e8a808691f9455c93306d5be44
Rigoberto0114 <rigobertodelgado1410@gmail.com>
2025-02-27 02:17:38 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 document.getElementById("btnPlanes").addEventListener("click", function() {
     let planes = document.getElementById("planes");
     if (planes.classList.contains("oculto")) {
-        planes.classList.remove("oculto");
-        planes.style.opacity = 1;  // Se muestra la sección de planes
-        setTimeout(() => {
-            document.querySelectorAll('.plan').forEach((plan, index) => {
-                setTimeout(() => {
-                    plan.classList.add('zoom-active');
-                }, index * 200);  // Aparecerán uno por uno
-            });
-        }, 300);  // Espera antes de empezar a mostrar los planes
+        planes.classList.remove("oculto");  // Muestra la sección de planes
     } else {
-        planes.classList.add("oculto");
-        planes.style.opacity = 0;  // Se oculta la sección de planes
+        planes.classList.add("oculto");  // Oculta la sección de planes
     }
 });
