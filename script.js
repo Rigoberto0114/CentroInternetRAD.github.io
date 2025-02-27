0597d8ef02428a3ea2e5d5200ad7d46ce1387ac6
Rigoberto0114 <rigobertodelgado1410@gmail.com>
2025-02-27 01:49:20 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 document.getElementById("btnPlanes").addEventListener("click", function() {
     let planes = document.getElementById("planes");
     if (planes.classList.contains("oculto")) {
-        planes.classList.remove("oculto");
+        planes.classList.remove("oculto");  // Muestra la sección de planes
     } else {
-        planes.classList.add("oculto");
+        planes.classList.add("oculto");  // Oculta la sección de planes
     }
 });
