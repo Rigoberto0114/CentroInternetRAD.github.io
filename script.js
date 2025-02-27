540bacd0a9db0af83ec810fb0124e5e4fcf4cbc8
Rigoberto0114 <rigobertodelgado1410@gmail.com>
2025-02-26 23:28:57 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function mostrarMensaje() {
-    alert("¡Gracias por visitar Centro Internet R.A.D!");
+function comprar() {
+    alert("¡Tu pedido ha sido agregado al carrito!");
+}
+
+function realizarPago() {
+    let metodo = prompt("¿Cómo deseas pagar? (Yappy, PayPal, Transferencia)");
+    if (metodo) {
+        alert("Redirigiendo a " + metodo + "...");
+    } else {
+        alert("Método de pago no seleccionado.");
+    }
 }
